944faca28699e095dde1dc23446b8af60a18bfb2
Muhye aldeen <86913045+MuhyeAlRaie@users.noreply.github.com>
2023-12-03 11:02:18 +0300

    edit main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// Sample JSON data for apartment details
+var apartmentData = {
+    "apartment1": {
+        "video": "video1.mp4",
+        "image": "asset/FloorPlan1.jpg",
+        "price": "$250,000",
+        "surface": "50 m²",
+        "availability": "Available"
+    },
+    // Add details for other apartments as needed
+};
+
 function changeVideo(videoSource, button) {
     document.getElementById('background-video').src = videoSource;
 
@@ -26,28 +38,32 @@ function openSidebar(type, button) {
     button.classList.add('active');
 
     if (type === 'apartments') {
-        // Change video source and add buttons for apartments
+        // Change video source and dynamically generate buttons for apartments
         changeVideo('asset/HomeVid.mp4', button);
-        for (var i = 1; i <= 10; i++) {
-            var apartmentButton = document.createElement('button');
-            apartmentButton.textContent = 'Apartment' + (i < 10 ? '0' + i : i);
-            apartmentButton.onclick = (function (index) {
-                return function () {
-                    changeVideo('video' + index + '.mp4', button);
-                    showApartmentDetails(index);
-                };
-            })(i);
-            sidebar.appendChild(apartmentButton);
+
+        // Loop through the apartmentData and generate buttons
+        for (var apartmentKey in apartmentData) {
+            if (apartmentData.hasOwnProperty(apartmentKey)) {
+                var apartmentButton = document.createElement('button');
+                apartmentButton.textContent = apartmentKey;
+                apartmentButton.onclick = (function (key) {
+                    return function () {
+                        changeVideo(apartmentData[key].video, button);
+                        showApartmentDetails(key);
+                    };
+                })(apartmentKey);
+                sidebar.appendChild(apartmentButton);
+            }
         }
     } else if (type === 'home') {
         // Change video source and add custom content for Home
         changeVideo('asset/HomeVid.mp4', button);
         sidebar.innerHTML = '<h2>Your Project on Earth</h2>' +
-            '<p>We are developing a sophisticated complex tailored for individuals who appreciate contemporary urban living. This innovative concept is designed to impeccably align with the distinctive standards of Dubai.</p>' +
-            '<h3>Public Places Nearby</h3>' +
-            '<p>4<br> minutes to the bus station</p>' +
-            '<p>7<br> minutes to the shopping center</p>' +
-            '<p>25 <br>minutes from the airport</p>';
+        '<p>We are developing a sophisticated complex tailored for individuals who appreciate contemporary urban living. This innovative concept is designed to impeccably align with the distinctive standards of Dubai.</p>' +
+        '<h3>Public Places Nearby</h3>' +
+        '<p>4<br> minutes to the bus station</p>' +
+        '<p>7<br> minutes to the shopping center</p>' +
+        '<p>25 <br>minutes from the airport</p>';
     } else if (type === 'features') {
         // Change video source and add buttons for features
         changeVideo('asset/FeaturesVid.mp4', button);
@@ -67,20 +83,21 @@ function openSidebar(type, button) {
     sidebar.style.display = 'block';
 }
 
-function showApartmentDetails(apartmentIndex) {
+function showApartmentDetails(apartmentKey) {
     var secondSidebar = document.getElementById('second-sidebar');
     var apartmentImage = document.getElementById('apartment-image');
     var apartmentPrice = document.getElementById('apartment-price');
     var apartmentSurface = document.getElementById('apartment-surface');
     var apartmentAvailability = document.getElementById('apartment-availability');
 
-    // Set apartment details
-    // apartmentImage.src = 'apartment' + apartmentIndex + '.jpg'; // Replace with actual image path
-    apartmentImage.src = 'asset/FloorPlane.jpg'; // Replace with actual image path
+    // Get the apartment details from the JSON data
+    var apartmentDetails = apartmentData[apartmentKey];
 
-    apartmentPrice.textContent = 'Price: $250,000'; // Replace with actual price
-    apartmentSurface.textContent = 'Surface: 50 m²'; // Replace with actual surface area
-    apartmentAvailability.textContent = 'Availability: Available'; // Replace with actual availability status
+    // Set apartment details
+    apartmentImage.src = apartmentDetails.image;
+    apartmentPrice.textContent = 'Price: ' + apartmentDetails.price;
+    apartmentSurface.textContent = 'Surface: ' + apartmentDetails.surface;
+    apartmentAvailability.textContent = 'Availability: ' + apartmentDetails.availability;
 
     // Show the second sidebar
     secondSidebar.style.display = 'block';
