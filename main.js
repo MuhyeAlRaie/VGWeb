dd88c486fa987433e0b349c563a73f0f37035a6f
Muhye aldeen <86913045+MuhyeAlRaie@users.noreply.github.com>
2023-12-09 04:03:51 +0300

    add filter apartments Features in Apartment section

--- main.js
+++ main.js
@@ -3,23 +3,24 @@ var apartmentData = {
     "apartment1": {
         "video": "asset/apartmentsVid.mp4",
         "image": "asset/FloorPlane.jpg",
-        "price": "$250,000",
-        "surface": "60 m²",
+        "price": 250000,
+        "surface": 60,
         "availability": "Available",
-        "bedroom": "2",
-        "bathroom":"1"
+        "bedroom": 2,
+        "bathroom": 1
     },
     "apartment2": {
         "video": "asset/apartmentsVid.mp4",
         "image": "asset/FloorPlane.jpg",
-        "price": "$200,000",
-        "surface": "50 m²",
+        "price": 200000,
+        "surface": 50,
         "availability": "Reserved",
-        "bedroom": "2",
-        "bathroom":"1"
+        "bedroom": 2,
+        "bathroom": 1
     },
     // Add details for other apartments as needed
 };
+
 
 function changeVideo(videoSource, button) {
     document.getElementById('background-video').src = videoSource;
@@ -51,7 +52,19 @@ function openSidebar(type, button) {
     if (type === 'apartments') {
         // Change video source and dynamically generate buttons for apartments
         changeVideo('asset/apartmentsVid.mp4', button);
-
+        sidebar.innerHTML ='<div id="filter-section" class="position-relative top-20 start-50 translate-middle-x z-index-1 d-grid">'+
+        '<label for="filter-price">Price:</label>'+
+        '<input type="text" id="filter-price" placeholder="Enter price">'+
+       ' <label for="filter-surface">Surface:</label>'+
+        '<input type="text" id="filter-surface" placeholder="Enter surface">'+
+        '<label for="filter-availability">Availability:</label>'+
+        '<select id="filter-availability">'+
+           ' <option value="">All</option>'+
+            '<option value="Available">Available</option>'+
+            '<option value="Reserved">Reserved</option>'+
+        '</select>'+
+        '<button onclick="applyFilters()">Apply Filters</button>'+
+    '</div>';
         // Loop through the apartmentData and generate buttons
         for (var apartmentKey in apartmentData) {
             if (apartmentData.hasOwnProperty(apartmentKey)) {
@@ -70,10 +83,10 @@ function openSidebar(type, button) {
         // Change video source and add custom content for Home
         changeVideo('asset/HomeVid.mp4', button);
         sidebar.innerHTML = '<h2>Your Project on Earth</h2>' +
-        '<p>We are developing a sophisticated complex tailored for individuals who appreciate contemporary urban living. This innovative concept is designed to impeccably align with the distinctive standards of Dubai.</p>' +
-        '<p>4<br> minutes to the bus station</p>' +
-        '<p>7<br> minutes to the shopping center</p>' +
-        '<p>25 <br>minutes from the airport</p>';
+            '<p>We are developing a sophisticated complex tailored for individuals who appreciate contemporary urban living. This innovative concept is designed to impeccably align with the distinctive standards of Dubai.</p>' +
+            '<p>4<br> minutes to the bus station</p>' +
+            '<p>7<br> minutes to the shopping center</p>' +
+            '<p>25 <br>minutes from the airport</p>';
     } else if (type === 'features') {
         // Change video source and add buttons for features
         changeVideo('asset/FeaturesVid.mp4', button);
@@ -88,18 +101,18 @@ function openSidebar(type, button) {
             };
             sidebar.appendChild(featureButton);
         }
-    }else if (type === 'contact') {
+    } else if (type === 'contact') {
         // Change video source and add buttons for features
         changeVideo('asset/Bgvid.mp4', button);
         sidebar.innerHTML = '<h2>CONTACT US</h2>' +
-        '<p>We are excited to hear from you and assist with any inquiries or feedback you may have. Please do not hesitate to reach out through one of the following methods:</p>' +
-        '<h3>Our Address</h3>' +
-        '<p>Queen Rania Street,Amman ,Jordan</p>' +
-        '<h3>Email Us</h3>' +
-        '<p>MR@VORTIGATE.COM</p>' +
-        '<h3>Call Us</h3>' +
-        '<p>+966 50 971 1777</p>'+
-        '<p>+962 781313782</p>';
+            '<p>We are excited to hear from you and assist with any inquiries or feedback you may have. Please do not hesitate to reach out through one of the following methods:</p>' +
+            '<h3>Our Address</h3>' +
+            '<p>Queen Rania Street,Amman ,Jordan</p>' +
+            '<h3>Email Us</h3>' +
+            '<p>MR@VORTIGATE.COM</p>' +
+            '<h3>Call Us</h3>' +
+            '<p>+966 50 971 1777</p>' +
+            '<p>+962 781313782</p>';
     }
     sidebar.style.display = 'block';
 }
@@ -113,17 +126,89 @@ function showApartmentDetails(apartmentKey) {
     var apartmentBedroom = document.getElementById('apartment-bedroom');
     var apartmentBathroom = document.getElementById('apartment-bathroom');
 
+    var filterSection = document.getElementById('filter-section');
+    var sidebar = document.getElementById('sidebar');
+
     // Get the apartment details from the JSON data
     var apartmentDetails = apartmentData[apartmentKey];
 
     // Set apartment details
     apartmentImage.src = apartmentDetails.image;
-    apartmentPrice.textContent = 'Price: ' + apartmentDetails.price;
-    apartmentSurface.textContent = 'Surface: ' + apartmentDetails.surface;
+    apartmentPrice.textContent = 'Price: $' + apartmentDetails.price.toLocaleString(); // Format price
+    apartmentSurface.textContent = 'Surface: ' + apartmentDetails.surface + ' m²';
     apartmentAvailability.textContent = 'Availability: ' + apartmentDetails.availability;
     apartmentBedroom.textContent = 'Bedroom: ' + apartmentDetails.bedroom;
     apartmentBathroom.textContent = 'Bathroom: ' + apartmentDetails.bathroom;
 
-    // Show the second sidebar
+    // Show the filter section
+    filterSection.style.display = 'flex';
+
+    // Hide the second sidebar
     secondSidebar.style.display = 'block';
+
+    // Hide the sidebar
+    sidebar.style.display = 'block';
+}
+
+// Function to apply filters and update the list of apartments
+function applyFilters() {
+    var filterPrice = parseFloat(document.getElementById('filter-price').value) || 0;
+    var filterSurface = parseFloat(document.getElementById('filter-surface').value) || 0;
+    var filterAvailability = document.getElementById('filter-availability').value;
+
+    var filteredApartments = {};
+
+    // Apply filters to the apartment data
+    for (var apartmentKey in apartmentData) {
+        if (apartmentData.hasOwnProperty(apartmentKey)) {
+            var apartment = apartmentData[apartmentKey];
+            if (
+                (filterPrice === 0 || apartment.price <= filterPrice) &&
+                (filterSurface === 0 || apartment.surface <= filterSurface) &&
+                (filterAvailability === "" || apartment.availability === filterAvailability)
+            ) {
+                filteredApartments[apartmentKey] = apartment;
+            }
+        }
+    }
+
+    // Update the sidebar with filtered apartments
+    updateSidebar(filteredApartments);
+}
+
+// Function to update the sidebar with the filtered apartments
+function updateSidebar(apartments) {
+    var sidebar = document.getElementById('sidebar');
+
+    sidebar.innerHTML ='<div id="filter-section" class="position-relative top-20 start-50 translate-middle-x z-index-1 d-grid">'+
+    '<label for="filter-price">Price:</label>'+
+    '<input type="text" id="filter-price" placeholder="Enter price">'+
+   ' <label for="filter-surface">Surface:</label>'+
+    '<input type="text" id="filter-surface" placeholder="Enter surface">'+
+    '<label for="filter-availability">Availability:</label>'+
+    '<select id="filter-availability">'+
+       ' <option value="">All</option>'+
+        '<option value="Available">Available</option>'+
+        '<option value="Reserved">Reserved</option>'+
+    '</select>'+
+    '<button onclick="applyFilters()">Apply Filters</button>'+
+'</div>';
+
+
+// Clear previous content
+    // sidebar.innerHTML = ''; 
+
+    for (var apartmentKey in apartments) {
+        if (apartments.hasOwnProperty(apartmentKey)) {
+            var apartmentButton = document.createElement('button');
+            apartmentButton.textContent = apartmentKey;
+            apartmentButton.onclick = (function (key) {
+                return function () {
+                    changeVideo(apartments[key].video, apartmentButton);
+                    showApartmentDetails(key);
+                };
+            })(apartmentKey);
+            sidebar.appendChild(apartmentButton);
+        }
+    }
 }
