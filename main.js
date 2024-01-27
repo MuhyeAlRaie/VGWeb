c3aa8248b7a8b7efa19626482d9bef781155d79a
yousef-maraqa <yousefmaraqa98@gmail.com>
2024-01-27 19:57:56 +0300

    bug fixes

--- main.js
+++ main.js
@@ -214,6 +214,7 @@ sidebar.appendChild(wrapperDiv);
 
 function showApartmentDetails(apartmentKey) {
     var secondSidebar = document.getElementById('second-sidebar');
+    var apartmentImage = document.getElementById('apartment-image');
     var apartmentPrice = document.getElementById('apartment-price');
     var apartmentSurface = document.getElementById('apartment-surface');
     var apartmentAvailability = document.getElementById('apartment-availability');
